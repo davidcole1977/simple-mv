75e2c609c4332dadb8f1d501ff9dee88ed3e176e
David Cole <hallo@davidcole.info>
2015-08-04 23:18:33 +0100

    refactored simple-validation.js, included adding message to validation options; reinstated object comparison helper function

--- test/mocha/simple-validation.spec.js
+++ test/mocha/simple-validation.spec.js
@@ -10,6 +10,14 @@
 
     describe('ValidatorList', function () {
       var validatorList,
+          everyType = [
+            '',
+            123,
+            {},
+            null,
+            [],
+            function () {}
+          ],
           everyThingExceptString = [
             123,
             {},
@@ -31,77 +39,99 @@
             [],
             function () {}
           ],
+          everyThingExceptArray = [
+            123,
+            'foo',
+            null,
+            {},
+            function () {}
+          ],
           dummyValidatorFunction = function () {},
-          anotherDummyValidatorFunction = function () {};
+          anotherDummyValidatorFunction = function () {},
+          validatorOptions1 = {
+            name: 'foo',
+            func: dummyValidatorFunction,
+            message: 'bar'
+          },
+          storedValidator1 = {
+            func: dummyValidatorFunction,
+            message: 'bar'
+          },
+          overwriteValidatorOptions = {
+            name: 'foo',
+            func: anotherDummyValidatorFunction,
+            message: 'foo'
+          },
+          storedOverwrittenValidator = {
+            func: anotherDummyValidatorFunction,
+            message: 'foo'
+          },
+          validatorOptions2 = {
+            name: 'bar',
+            func: anotherDummyValidatorFunction,
+            message: 'foo'
+          },
+          storedValidator2 = {
+            func: anotherDummyValidatorFunction,
+            message: 'foo'
+          },
+          exampleValidatorList = {
+            foo: dummyValidatorFunction,
+            bar: anotherDummyValidatorFunction
+          };
 
       beforeEach(function () {
         validatorList = module.create();
       });
 
       describe('create()', function () {
-
         it('sets default validators', function () {
-          var newValidatorList = module.create({
-                foo: dummyValidatorFunction,
-                bar: anotherDummyValidatorFunction
-              });
-
+          var newValidatorList = module.create([validatorOptions1, validatorOptions2]);
           expect(newValidatorList.validators).to.deep.equal({
-            foo: dummyValidatorFunction,
-            bar: anotherDummyValidatorFunction
+            foo: storedValidator1,
+            bar: storedValidator2
           });
-
         });
 
-        it('throws error if argument is not an object', function () {
-          everyThingExceptObject.forEach(function(variable) {
+        it('throws error if argument is not an array', function () {
+          everyThingExceptArray.forEach(function(variable) {
             expect(module.create.bind(null, variable)).to.throw(Error);
           });
         });
 
-        it('throws error if any parameter value in the argument object is not a function', function () {
-          everyThingExceptFunction.forEach(function(variable) {
-            expect(module.create.bind(null, {foo: function(){}, bar: variable})).to.throw(Error);
+        it('throws error if a value in the argument array is not a valid validator options object', function () {
+          everyType.forEach(function(variable) {
+            expect(module.create.bind(null, [validatorOptions1, variable])).to.throw(Error);
           });
         });
 
         describe('set()', function () {
-          it('sets a new validator function', function () {
-            validatorList.set('foo', dummyValidatorFunction);
-            expect(validatorList.validators.foo).to.equal(dummyValidatorFunction);
+          it('sets a new validator object', function () {
+            validatorList.set(validatorOptions1);
+            expect(validatorList.validators.foo).to.deep.equal(storedValidator1);
           });
 
-          it('overwrites an existing validator function', function () {
-            validatorList.validators.foo = dummyValidatorFunction;
-            validatorList.set('foo', anotherDummyValidatorFunction);
-            expect(validatorList.validators.foo).to.equal(anotherDummyValidatorFunction);
+          it('overwrites an existing validator object', function () {
+            validatorList.validators.foo = storedValidator1;
+            validatorList.set(overwriteValidatorOptions);
+            expect(validatorList.validators.foo).to.deep.equal(storedOverwrittenValidator);
           });
 
-          it('throws an error when first argument is not a string', function () {
-            everyThingExceptString.forEach(function(variable) {
-              expect(validatorList.set.bind(validatorList, variable, dummyValidatorFunction)).to.throw(Error);
+          it('throws an error when no arguments are received', function () {
+            expect(validatorList.set.bind(validatorList)).to.throw(Error);
+          });
+
+          it('throws an error when argument is not a validator options-like object', function () {
+            everyType.forEach(function(variable) {
+              expect(validatorList.set.bind(validatorList, variable)).to.throw(Error);
             });
-          });
-
-          it('throws an error when first argument is a string with a length of zero', function () {
-            expect(validatorList.set.bind(validatorList, '', dummyValidatorFunction)).to.throw(Error);
-          });
-
-          it('throws an error when second argument is not a function', function () {
-            everyThingExceptFunction.forEach(function(variable) {
-              expect(validatorList.set.bind(validatorList, 'foo', variable)).to.throw(Error);
-            });
-          });
-
-          it('throws an error when fewer than two arguments are received', function () {
-            expect(validatorList.set.bind(validatorList, 'foo')).to.throw(Error);
           });
         });
 
         describe('get()', function () {
-          it('returns an existing validator function', function () {
-            validatorList.validators.foo = dummyValidatorFunction;
-            expect(validatorList.get('foo')).to.equal(dummyValidatorFunction);
+          it('returns an existing validator object', function () {
+            validatorList.validators.foo = storedValidator1;
+            expect(validatorList.get('foo')).to.equal(storedValidator1);
           });
 
           it('returns null when requested validator doesn\'t exist', function () {
@@ -125,16 +155,16 @@
 
         describe('method tests using multiple default validators', function () {
           beforeEach(function () {
-            validatorList = module.create({
-              foo: dummyValidatorFunction,
-              bar: anotherDummyValidatorFunction
-            });
+            validatorList = module.create([
+              validatorOptions1,
+              validatorOptions2
+            ]);
           });
 
           describe('remove()', function () {
             it('removes one of the list of previously set validators', function () {
               validatorList.remove('bar');
-              expect(validatorList.validators).to.deep.equal({foo: dummyValidatorFunction});
+              expect(validatorList.validators).to.deep.equal({foo: storedValidator1});
             });
 
             it('throws an error when argument is not a string', function () {
@@ -162,8 +192,8 @@
           describe('getAll()', function () {
             it('gets all of the list of previously set validators', function () {
               expect(validatorList.getAll()).to.deep.equal({
-                foo: dummyValidatorFunction,
-                bar: anotherDummyValidatorFunction
+                foo: storedValidator1,
+                bar: storedValidator2
               });
             });
           });
