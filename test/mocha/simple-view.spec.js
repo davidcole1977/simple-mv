65471190a2b78dd5661a0369365e32257eada247
David Cole <hallo@davidcole.info>
2015-08-15 21:51:43 +0100

    started to flesh out tests / specs for simple-view.js model binding and listening

--- test/mocha/simple-view.spec.js
+++ test/mocha/simple-view.spec.js
@@ -5,7 +5,8 @@
       sinon = require('sinon'),
       utHelpers = require('../lib/unit-test-helpers.js'),
       module = require(utHelpers.getModulePath('simple-view')),
-      modelSubs = require(utHelpers.getModulePath('model-subscriptions'));
+      modelSubs = require(utHelpers.getModulePath('model-subscriptions')),
+      sm = require(utHelpers.getModulePath('simple-model'));
 
   
   describe('simple-view', function () {
@@ -40,11 +41,26 @@
       });
     });
 
+    xdescribe('getModel', function () {
+      it('retrieves model by ID string', function () {
+
+      });
+
+      it('retrieves model by object reference', function () {
+
+      });
+
+      it('validates arguments', function () {
+
+      });
+    });
+
     describe('prototype methods', function () {
-      var view;
+      var view, model;
 
       beforeEach(function () {
-        view = view.create();
+        view = module.create();
+        model = sm.create();
       });
 
       xdescribe('method()', function () {
@@ -54,34 +70,115 @@
       });
 
       xdescribe('bind()', function () {
-        it('binds all model data to data parameter if a single argument (model object / id string) is received', function () {
-        
+
+        describe('binding a whole single model to the view data (one argument: model object / ID string)', function () {
+          it('view data reflects state of model with no data', function () {
+            view.bind(model);
+
+            expect(view.data).to.deep.equal({});
+          });
+
+          it('view data reflects state of model with data', function () {
+            model.set('foo', 'boo');
+            model.set('bar', 'car');
+
+            view.bind(model);
+
+            expect(view.data).to.deep.equal({
+              foo: 'boo',
+              bar: 'car'
+            });
+          });
+
+          it('binds by model id string instead of model object reference', function () {
+            model.set('foo', 'boo');
+            model.set('bar', 'car');
+
+            view.bind(model.id);
+
+            expect(view.data).to.deep.equal({
+              foo: 'boo',
+              bar: 'car'
+            });
+          });
+
+          it('view data updates correspondingly when model data updates', function () {
+            model.set('foo', 'boo');
+            model.set('bar', 'car');
+
+            view.bind(model);
+
+            model.set('foo', 'hoo');
+            model.set('bar', 'far');
+
+            expect(view.data).to.deep.equal({
+              foo: 'hoo',
+              bar: 'far'
+            });
+          });
         });
 
-        it('binds all model data to a named parameter if two arguments are received (model, parameter name)', function () {
-        
+        xdescribe('binding a whole model to a named view data parameter (two arguments: model, parameter name) ', function () {
+          it('stuff', function () {
+          
+          });
         });
 
-        it('binds model datum to a named parameter if three arguments are received (model, datum, parameter name)', function () {
+        // it('binds all model data to a named parameter if two arguments are received (model, parameter name)', function () {
         
+        // });
+
+        xdescribe('binding a model datum to a named view data parameter (three arguments: model, datum, parameter namee) ', function () {
+          it('stuff', function () {
+          
+          });
         });
+
+        xdescribe('validate arguments', function () {
+          it('stuff', function () {
+          
+          });
+        });
+
+        // it('binds model datum to a named parameter if three arguments are received (model, datum, parameter name)', function () {
+        
+        // });
+
+        
+
+        
       });
 
-     xdescribe('addListener()', function () {
-        it('listen to model', function () {
-        
+      xdescribe('addListener()', function () {
+
+        describe('listening to a model', function () {
+          it('stuff', function () {
+          
+          });
         });
 
-        it('listen to model / event', function () {
-        
+        describe('listening to a model by event', function () {
+          it('stuff', function () {
+          
+          });
         });
 
-        it('listen to model / datum', function () {
-        
+        describe('listening to a specific datum on a model', function () {
+          it('stuff', function () {
+          
+          });
         });
 
-        it('listen to model / datum / event', function () {
-        
+        describe('listening to a specific datum on a model by event', function () {
+          it('stuff', function () {
+          
+          });
+        });
+
+        describe('arguments validation', function () {
+          it('stuff', function () {
+          
+          });
         });
       });
 
